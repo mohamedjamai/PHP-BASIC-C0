7f4efe9ec19c85802fbe3eeb8e458d262918d3b4
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-27 15:04:49 +0100

    vardump (2.11)

---
+++ 2-Variabelen-en-datatype/2.11 - var_dump()/Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROC AMSTELLAND</title>
+</head>
+<body>
+        <ul>
+        <?php
+              $a = "Mohamed";
+              echo var_dump($a) . "<br>";
+
+              $b = 17;
+              echo var_dump($b) . "<br>";
+
+        ?>
+        </ul>
+</body>
+</html>
