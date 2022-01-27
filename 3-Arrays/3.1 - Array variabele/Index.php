0cc76ccd47ee15b8a5573f099964050617689054
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-27 15:08:13 +0100

    array variable (3.1)

---
+++ 3-Arrays/3.1 - Array variabele/Index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP BEGINNING</title>
+</head>
+<body>
+    <h1> PHP BEGINNING </h1>
+
+<ul>
+<?php
+        $myMovies = ["<li>Spiderman</li>", "<li>Batman</li>", "<li>The Joker</li>"];
+
+        foreach ($myMovies as $myMovie) {
+        echo $myMovie;
+        echo '<br>';
+        }
+?>
+</ul>
+</body>
+</html>
