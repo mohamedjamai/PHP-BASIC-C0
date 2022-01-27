c6ae73b96bfa65bdc0360cb4a08a6a40dc47bbfd
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-27 14:43:22 +0100

    datatype (2.7)

---
+++ 2-Variabelen-en-datatype/2.7 - data en tekst combineren/Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROC AMSTELLAND</title>
+</head>
+<body>
+        <?php
+                $voornaam = "Mohamed";
+                $leeftijd = "17";
+
+                print "Mijn naam is $voornaam en ik ben $leeftijd jaar oud"
+                
+
+        ?>
+
+</body>
+</html>
