62fc0336ad0fe32c2b3fd70405d1b6a9a852671a
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-27 14:39:40 +0100

    gettype (2.6)

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/Index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROC AMSTELLAND</title>
+</head>
+<body>
+        <?php
+                $voornaam = "Tom";
+                echo gettype($voornaam); 
+               
+                $value = '1234';
+                echo "'1234' Is data type - ".gettype($value);
+
+                $value = true;
+                echo "true Is data type - ".gettype($value);
+
+                $value = null;
+                echo "null Is data type - ".gettype($value);
+
+                $value = 10.365;
+                echo " 10.365 Is data type - ".gettype($value);
+
+
+        ?>
+
+</body>
+</html>
