b13068b91b49ed7fbc0b981cd44a45089a99b51e
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-27 15:01:46 +0100

    datatype (2.10)

---
+++ 2-Variabelen-en-datatype/2.10 - String concatenation 2/Index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROC AMSTELLAND</title>
+</head>
+<body>
+        <ul>
+        <?php
+               $item = "<li>iPhone</li> ";
+               $item1 = "<li>Oplader</li>";
+               $item2 = "<li>Oortjes</li>";
+               $item .= $item1 . $item2;
+
+               echo $item
+
+        ?>
+        </ul>
+</body>
+</html>
