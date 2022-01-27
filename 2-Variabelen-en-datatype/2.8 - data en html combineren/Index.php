5fa19d245dfe60ed1107ed1d2949ab23de02f9a5
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-27 14:48:21 +0100

    datatype (2.8)

---
+++ 2-Variabelen-en-datatype/2.8 - data en html combineren/Index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROC AMSTELLAND</title>
+</head>
+<body>
+        <ul>
+        <?php
+               $voornaam = "Mohamed Azekri";
+               $voornaamm = "Youssef";
+               $voornaammm = "Bilal";
+
+               echo "<li>$voornaam</li><li>$voornaamm</li><li>$voornaammm</li>"
+                
+
+        ?>
+        </ul>
+</body>
+</html>
