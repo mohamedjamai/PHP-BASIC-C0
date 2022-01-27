4a727ab78ccf751603e6cdb3372f80318ac20ee4
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-27 14:53:29 +0100

    datatype (2.9)

---
+++ 2-Variabelen-en-datatype/2.9 - String concatenation/Index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROC AMSTELLAND</title>
+</head>
+<body>
+        <ul>
+        <?php
+               $voornaam = "Mohamed";
+               $leeftijd = "17";
+              
+
+               echo "Jouw naam is $voornaam, en je bent $leeftijd jaar oud <br>";  
+               echo "Waar hierboven xxx staat moet natuurlijk jouw echte naam en leeftijd staan!";
+                
+
+        ?>
+        </ul>
+</body>
+</html>
