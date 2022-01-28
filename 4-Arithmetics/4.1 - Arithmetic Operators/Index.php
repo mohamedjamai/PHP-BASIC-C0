e21492ff7126ba28b0e8ef28cade457b70d30581
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-28 17:11:24 +0100

    4

---
+++ 4-Arithmetics/4.1 - Arithmetic Operators/Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP BEGINNING</title>
+</head>
+<body>
+    <h1> PHP BEGINNING </h1>
+
+<ul>
+<?php
+$getal_1 = 1;
+$getal_2 = 2;
+echo $getal_1+$getal_2;
+?>
+</ul>
+</body>
+</html>
