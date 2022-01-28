443775983bbc86f5b928d63b696cdcc371a24122
mohamedjamai <90444195+mohamedjamai@users.noreply.github.com>
2022-01-28 17:11:29 +0100

    3

---
+++ 3-Arrays/3.5 - Array sorteren/Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP BEGINNING</title>
+</head>
+<body>
+    <h1> PHP BEGINNING </h1>
+
+<ul>
+<?php
+$getallen = [676, 5345, 78657, 3, 46, 21, 47];
+asort($getallen);
+$laag_hoog = $getallen;
+print_r($laag_hoog);
+?>
+</ul>
+</body>
+</html>
